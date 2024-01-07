b10fbafa65a6313ccd3254125cc3c51ebedc4f7f
Playground <info@playgroundengineer.com>
2024-01-07 14:05:47 -0600

    Initial Commit

---
+++ MusicTracker/MusicTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTrackerApp.swift
+//  MusicTracker
+//
+//  Created by Mekhala Vithala on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
