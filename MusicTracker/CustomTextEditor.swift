6302a9c57e20250b243adcf88957b60fea551a74
tanviaemireddy <148006632+tanviaemireddy@users.noreply.github.com>
2024-04-21 13:48:40 -0500

    Create CustomTextEditor.swift
    Layout for text fields in WriteView

---
+++ MusicTracker/CustomTextEditor.swift
@@ -0,0 +1,45 @@
+
+import SwiftUI
+
+struct CustomTextEditor: View {
+    @State var text = ""
+    @State private var isSheetPresented = false
+    
+    
+    var body: some View {
+        VStack {
+            TextField("Enter your text", text: $text)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onTapGesture {
+                    isSheetPresented = true
+                }
+        }
+        .sheet(isPresented: $isSheetPresented) {
+            EditSheet(text: $text, isSheetPresented: $isSheetPresented)
+        }
+    }
+}
+
+struct EditSheet: View {
+    @Binding var text: String
+    @Binding var isSheetPresented: Bool
+    
+    var body: some View {
+        VStack {
+            TextEditor(text: $text)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Done") {
+                isSheetPresented = false
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+
+
+
