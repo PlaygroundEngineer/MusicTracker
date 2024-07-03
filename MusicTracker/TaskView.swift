c2be89e5c7a7026ee521bea0f4cac7e403dbf676
hansikaGanga <148006544+hansikaGanga@users.noreply.github.com>
2024-07-02 21:45:07 -0500

    Update TaskView.swift

--- MusicTracker/TaskView.swift
+++ MusicTracker/TaskView.swift
@@ -5,6 +5,10 @@ struct TaskView: View {
     @State private var isImagePickerPresented: Bool = false
     @Binding var newEntry: PracticeEntry
     @ObservedObject var entryManager: EntryManager
+    @State private var timer: Timer? = nil
+    @State private var isPlaying: Bool = false
+    @State private var elapsedTime: TimeInterval = 0
+    @State private var customText: String = ""
     
     var body: some View {
         HStack {
@@ -27,27 +31,47 @@ struct TaskView: View {
             
             ZStack(alignment: .leading) {
                 ZStack {
-                    Rectangle()
+                    RoundedRectangle(cornerRadius: 25)
                         .frame(width: 110, height: 25)
-                        .cornerRadius(25)
                         .foregroundColor(Color.white)
-                    
-                    Text("  25 minutes")
-                        .font(.system(size: 10))
+                    if isPlaying {
+                        Text("\(Int(elapsedTime)) seconds")
+                            .foregroundColor(.black)
+                            .padding(.horizontal) // Add horizontal padding
+                            .frame(width: 85, height: 40) // Fixed width
+                            .font(.system(size: 10))
+                    } else {
+                        TextField("Type here", text: $customText)
+                            .foregroundColor(.black)
+                            .padding(.horizontal) // Add horizontal padding
+                            .frame(width: 85, height: 40) // Fixed width
+                            .font(.system(size: 10))
+                        
+                    }
                 }
-                
                 ZStack {
                     Circle()
                         .frame(width: 25)
                         .foregroundColor(Color(hex: CustomColors.black, opacity: 1))
-                    Image(systemName: "play.fill")
-                        .font(.system(size: 10))
-                        .foregroundColor(Color.white)
+                    
+                    Button(action: {
+                        if isPlaying {
+                            stopTimer()
+                        } else {
+                            startTimer()
+                        }
+                        isPlaying.toggle()
+                    }) {
+                        Image(systemName: isPlaying ? "stop.fill" : "play.fill")
+                            .font(.system(size: 10))
+                            .foregroundColor(Color.white)
+                    }
                 }
             }
             
             ZStack {
                 Button {
+                    newEntry.duration = Int(elapsedTime)
                     newEntry.imageData = selectedImage?.jpegData(compressionQuality: 1.0)
                     newEntry.colorHex = entryManager.getSequentialColor()
                     entryManager.entries
@@ -79,5 +103,17 @@ struct TaskView: View {
             return
         }
         self.selectedImage = selectedImage
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            elapsedTime += 1
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
     }
 }
