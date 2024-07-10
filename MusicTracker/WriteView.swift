f11ce95f555c01a6781b8297c795a8b075f5b101
hansikaGanga <148006544+hansikaGanga@users.noreply.github.com>
2024-07-09 21:21:36 -0500

    Update WriteView.swift

--- MusicTracker/WriteView.swift
+++ MusicTracker/WriteView.swift
@@ -60,51 +60,53 @@ struct WriteView: View {
     
     var body: some View {
         Color(hex: CustomColors.cream, opacity: 1)
-            .edgesIgnoringSafeArea(.all)
-            .overlay(
-                GeometryReader { geometry in
-                    ZStack {
-                        VStack (spacing: 1) {
-                            Spacer()
-                            Text("journey")
-                                .font(.system(size: 40))
-                            //.fontDesign(.monospaced)
-                            //.fontWeight(.light)
-                            Text("FURTHER")
-                                .font(.system(size: 50))
-                            //.fontDesign(.serif)
-                            //.fontWeight(.bold)
-                                .zIndex(1)
+         .edgesIgnoringSafeArea(.all)
+         .overlay(
+            GeometryReader { geometry in
+                ZStack {
+                    VStack (spacing: 1) {
+                        Spacer()
+                        Text("journey")
+                            .font(.system(size: 40))
+                            .fontDesign(.monospaced)
+                            .fontWeight(.light)
+                            .foregroundColor(Color(hex: CustomColors.black))
+                        Text("FURTHER")
+                            .font(.system(size: 50))
+                            .fontDesign(.serif)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hex: CustomColors.black))
+                            .zIndex(1)
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(Color(hex: CustomColors.tan, opacity: 1))
+                                .frame(width: UIScreen.width * 0.9, alignment: .center)
+                                .offset(y: -25)
+                                .zIndex(0)
                             
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(Color(hex: CustomColors.tan, opacity: 1))
-                                    .frame(width: UIScreen.width * 0.9, alignment: .center)
-                                    .offset(y: -25)
-                                    .zIndex(0)
+                            VStack {
+                                // Song Title Text Editor
+                                TextEditorWithPlaceholder(text: $newEntry.songTitle, placeholder: "Type your song...")
+                                    .frame(width: UIScreen.width * 0.84)
                                 
-                                VStack {
-                                    // Song Title Text Editor
-                                    TextEditorWithPlaceholder(text: $newEntry.songTitle, placeholder: "Type your song...")
-                                        .frame(width: UIScreen.width * 0.84)
-                                    
-                                    // Feedback Text Editor
-                                    TextEditorWithPlaceholder(text: $newEntry.notes, placeholder: "Type your practice...")
-                                        .frame(width: UIScreen.width * 0.84)
-                                    
-                                    // Notes Text Editor
-                                    TextEditorWithPlaceholder(text: $newEntry.feedback, placeholder: "Type your feedback...")
-                                        .frame(width: UIScreen.width * 0.84)
-                                    TaskView(newEntry: $newEntry, entryManager: entryManager)
-                                }
-                                .padding(8)
+                                // Feedback Text Editor
+                                TextEditorWithPlaceholder(text: $newEntry.notes, placeholder: "Type your practice...")
+                                    .frame(width: UIScreen.width * 0.84)
+                                
+                                // Notes Text Editor
+                                TextEditorWithPlaceholder(text: $newEntry.feedback, placeholder: "Type your feedback...")
+                                    .frame(width: UIScreen.width * 0.84)
+                                TaskView(newEntry: $newEntry, entryManager: entryManager)
                             }
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(8)
                         }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
                     }
                 }
-            )
+            }
+         )
     }
 }
 
@@ -127,6 +129,4 @@ struct TextEditorWithPlaceholder: View {
         }
         .padding(8)
     }
-
-} 
-
+}
