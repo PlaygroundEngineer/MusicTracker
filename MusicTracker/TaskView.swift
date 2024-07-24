e873ff0a116a3685158967f4dedfaab47d986d6e
tanviaemireddy <148006632+tanviaemireddy@users.noreply.github.com>
2024-07-24 15:29:33 -0500

    Update TaskView.swift
    Made TaskView larger, and also changed functionality of add button.
    Please test code to see if it looks good on the iPhone as I’m unable to see it on that view.

--- MusicTracker/TaskView.swift
+++ MusicTracker/TaskView.swift
@@ -8,50 +8,42 @@ struct TaskView: View {
     @State private var timer: Timer? = nil
     @State private var isPlaying: Bool = false
     @State private var elapsedTime: TimeInterval = 0
-    @State private var customText: String = ""
     
     var body: some View {
-        HStack {
+        HStack(spacing: 20) { // Adjusted spacing for better proportion
+            // Image Picker Section
             ZStack {
                 Circle()
-                    .frame(width: 25)
-                    .foregroundColor(selectedImage == nil ? Color(hex: CustomColors.black) : Color(hex: CustomColors.cream))
+                    .frame(width: 40) // Increased size
+                    .foregroundColor(selectedImage == nil ? Color(hex: CustomColors.black) : Color(hex: CustomColors.white))
                 Button(action: {
                     isImagePickerPresented.toggle()
                 }) {
                     Image(systemName: selectedImage == nil ? "camera" : "checkmark")
-                        .font(.system(size: 10))
-                        .foregroundColor( selectedImage == nil ? Color(hex: CustomColors.cream) : Color(hex: CustomColors.black))
+                        .font(.system(size: 24)) // Increased font size
+                        .foregroundColor(selectedImage == nil ? Color(hex: CustomColors.white) : Color(hex: CustomColors.black))
                 }
-                .padding(4)
+                .padding(8)
                 .sheet(isPresented: $isImagePickerPresented, onDismiss: loadImage) {
                     ImagePicker(selectedImage: $selectedImage)
                 }
             }
             
+            // Timer Section
             ZStack(alignment: .leading) {
                 ZStack {
-                    RoundedRectangle(cornerRadius: 25)
-                        .frame(width: 110, height: 25)
-                        .foregroundColor(Color.white)
-                    if isPlaying {
-                        Text("\(Int(elapsedTime)) seconds")
-                            .foregroundColor(Color(hex: CustomColors.black))
-                            .padding(.horizontal) // Add horizontal padding
-                            .frame(width: 85, height: 40) // Fixed width
-                            .font(.system(size: 10))
-                    } else {
-                        TextField("Type here", text: $customText)
-                            .foregroundColor(Color(hex: CustomColors.black))
-                            .padding(.horizontal) // Add horizontal padding
-                            .frame(width: 85, height: 40) // Fixed width
-                            .font(.system(size: 10))
-                        
-                    }
+                    RoundedRectangle(cornerRadius: 30) // Increased corner radius
+                        .frame(width: 170, height: 40) // Increased size
+                        .foregroundColor(Color(hex: CustomColors.white))
+                    Text(formatElapsedTime(elapsedTime))
+                        .foregroundColor(Color(hex: CustomColors.black))
+                        .padding(.horizontal)
+                        .frame(width: 170, height: 40, alignment: .center) // Increased size
+                        .font(.system(size: 20)) // Increased font size
                 }
                 ZStack {
                     Circle()
-                        .frame(width: 25)
+                        .frame(width: 40) // Increased size
                         .foregroundColor(Color(hex: CustomColors.black, opacity: 1))
                     
                     Button(action: {
@@ -63,40 +55,42 @@ struct TaskView: View {
                         isPlaying.toggle()
                     }) {
                         Image(systemName: isPlaying ? "stop.fill" : "play.fill")
-                            .font(.system(size: 10))
-                            .foregroundColor(Color(hex: CustomColors.cream))
+                            .font(.system(size: 20)) // Increased font size
+                            .foregroundColor(Color(hex: CustomColors.white))
                     }
                 }
             }
             
+            // Add Button Section
             ZStack {
                 Button {
-                    newEntry.duration = Int(elapsedTime)
-                    newEntry.imageData = selectedImage?.jpegData(compressionQuality: 1.0)
-                    newEntry.colorHex = entryManager.getSequentialColor()
-                    entryManager.entries
-                        .append(newEntry)
-                    entryManager.saveEntriesToUserDefaults()
-                    selectedImage = nil
-                    elapsedTime = 0
-                    newEntry = PracticeEntry(imageData: nil, duration: 0, songTitle: "", feedback: "", notes: "", colorHex: entryManager.getSequentialColor())
+                    if !newEntry.songTitle.isEmpty || !newEntry.notes.isEmpty || !newEntry.feedback.isEmpty {
+                        newEntry.duration = formatElapsedTime(elapsedTime)
+                        newEntry.imageData = selectedImage?.jpegData(compressionQuality: 1.0)
+                        newEntry.colorHex = entryManager.getSequentialColor()
+                        entryManager.entries.append(newEntry)
+                        entryManager.saveEntriesToUserDefaults()
+                        selectedImage = nil
+                        elapsedTime = 0
+                        stopTimer()
+                        newEntry = PracticeEntry(imageData: nil, duration: "", songTitle: "", feedback: "", notes: "", colorHex: entryManager.getSequentialColor())
+                    }
                 } label: {
                     ZStack {
                         Rectangle()
-                            .frame(width: 60, height: 25)
-                            .cornerRadius(25)
+                            .frame(width: 100, height: 40) // Increased size
+                            .cornerRadius(30) // Increased corner radius
                             .foregroundColor(Color(hex: CustomColors.black, opacity: 1))
                         
                         Text("Add ->")
-                            .font(.system(size: 10))
-                            .foregroundColor(Color(hex: CustomColors.cream))
+                            .font(.system(size: 20)) // Increased font size
+                            .foregroundColor(Color(hex: CustomColors.white))
                     }
                 }
             }
         }
-        .padding(8)
-        
-        
+        .frame(width: UIScreen.width * 0.9, alignment: .center) // Adjusted width for proportion
+        .padding(20)
     }
     
     func loadImage() {
@@ -117,4 +111,12 @@ struct TaskView: View {
         timer?.invalidate()
         timer = nil
     }
+    
+    func formatElapsedTime(_ elapsedTime: TimeInterval) -> String {
+        let hours = Int(elapsedTime) / 3600
+        let minutes = (Int(elapsedTime) % 3600) / 60
+        let seconds = Int(elapsedTime) % 60
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
 }
+
